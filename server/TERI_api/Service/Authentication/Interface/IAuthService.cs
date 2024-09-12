1a71c55dc82f1db48214e908889c840ee3fd9eeb
ndrs <szaki1006@gmail.com>
2024-09-12 15:01:41 +0200

    :necktie: chore: implemented LoginAsync method in AuthService

--- server/TERI_api/Service/Authentication/Interface/IAuthService.cs
+++ server/TERI_api/Service/Authentication/Interface/IAuthService.cs
@@ -4,5 +4,6 @@ namespace TERI_api.Service.Authentication;
 
 public interface IAuthService
 {
-    Task<AuthResult> RegisterAsync(string email, string username, string password);
+    Task<AuthResult> RegisterAsync(string email, string username, string password, string role);
+    Task<AuthResult> LoginAsync(string email, string password);
 }
