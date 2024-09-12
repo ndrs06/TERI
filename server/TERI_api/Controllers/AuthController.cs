7d25075ee12508c6a6167114ff90fd63f3317f59
ndrs <szaki1006@gmail.com>
2024-09-12 15:04:21 +0200

    :passport_control: implemented sign-in endpoint into AuthController

--- server/TERI_api/Controllers/AuthController.cs
+++ server/TERI_api/Controllers/AuthController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using TERI_api.Model.Authentication;
+using TERI_api.Model.DataModel;
 using TERI_api.Service.Authentication;
+using TERI_api.Service.Interface.Serv;
 
 namespace TERI_api.Controllers;
 
@@ -9,21 +11,26 @@ namespace TERI_api.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly IAuthService _authenticationService;
+    private readonly IUserService _userService;
+    private readonly IConfiguration _configuration;
 
-    public AuthController(IAuthService authenticationService)
+    public AuthController(IAuthService authenticationService, IUserService userService, IConfiguration configuration)
     {
         _authenticationService = authenticationService;
+        _userService = userService;
+        _configuration = configuration;
     }
 
     [HttpPost("sign-up")]
-    public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+    public async Task<ActionResult<RegistrationResponse>> Register([FromBody] RegistrationRequest request)
     {
         if (!ModelState.IsValid)
         {
             return BadRequest(ModelState);
         }
 
-        var result = await _authenticationService.RegisterAsync(request.Email, request.Username, request.Password);
+        var userRole = _configuration.GetSection("JwtSettings").GetSection("Roles")["User"];
+        var result = await _authenticationService.RegisterAsync(request.Email, request.Username, request.Password, userRole);
 
         if (!result.Success)
         {
@@ -32,6 +39,29 @@ public class AuthController : ControllerBase
         }
 
         return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.UserName));
+    }
+    
+    [HttpPost("sign-in")]
+    public async Task<ActionResult<User>> Login([FromBody] LoginRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var result = await _authenticationService.LoginAsync(request.Email, request.Password);
+
+        if (!result.Success)
+        {
+            AddErrors(result);
+            return BadRequest(ModelState);
+        }
+
+        var token = result.Token;
+        
+        HttpContext.Response.Cookies.Append("access_token", token, new CookieOptions{HttpOnly = true});
+
+        return Ok(_userService.GetByEmail(request.Email));
     }
 
     private void AddErrors(AuthResult result)
