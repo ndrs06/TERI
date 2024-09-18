0918ea62c6461cbad837352717a7f38db39ba742
ndrs <szaki1006@gmail.com>
2024-09-18 12:13:18 +0200

    :sparkles: feat: extend main with new elements

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,23 +3,31 @@ import ReactDOM from 'react-dom/client'
 import './index.scss'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
+import {UserContextProvider} from "./contexts/UserContext.jsx";
 import Layout from './pages/Layout/Layout.jsx';
 import Home from "./pages/Home.jsx";
 import SignUp from "./pages/SignUp.jsx";
 import SignIn from "./pages/SignIn.jsx";
+import ProtectedRoutes_user from "./pages/ProtectedRoutes/ProtectedRoutes_user.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        
-        <Route path="/" element={<Layout />}>
-            <Route path="/" element={<Home />}/>
-            <Route path="/sign-up" element={<SignUp />}/>
-            <Route path="/sign-in" element={<SignIn />}/>
-        </Route>
-        
-      </Routes>
-    </BrowserRouter>
+      <UserContextProvider>
+        <BrowserRouter>
+          <Routes>
+            
+            <Route path="/" element={<Layout />}>
+                <Route path="/" element={<Home />}/>
+                <Route path="/sign-up" element={<SignUp />}/>
+                <Route path="/sign-in" element={<SignIn />}/>
+            </Route>
+              
+            <Route element={<ProtectedRoutes_user />}>
+                
+            </Route>  
+            
+          </Routes>
+        </BrowserRouter>
+      </UserContextProvider>
   </React.StrictMode>
 )
