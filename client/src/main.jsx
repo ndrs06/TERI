bb4386b98e40cf2746e040af31a59c450ecf6fee
ndrs <szaki1006@gmail.com>
2024-09-18 10:42:57 +0200

    :sparkles: feat: implemented pages to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.scss'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import Layout from './pages/Layout/Layout.jsx';
+import Home from "./pages/Home.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import SignIn from "./pages/SignIn.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />}/>
+            <Route path="/sign-up" element={<SignUp />}/>
+            <Route path="/sign-in" element={<SignIn />}/>
+        </Route>
+        
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
