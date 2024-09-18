d836f8ee5240300821bd73d6330c0689608d5942
ndrs <szaki1006@gmail.com>
2024-09-18 10:38:12 +0200

    :bricks: chore: created basic navbar component

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.scss';
+
+export default function Navbar() {
+    
+    return (
+        <nav className="navbar">
+            <ul className="navbar-links">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/sign-in">Sign In</Link>
+                </li>
+                <li>
+                    <Link to="/sign-up">Sign Up</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
