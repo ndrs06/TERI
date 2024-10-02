a9661fa550ad73c81e677efc5e3e7008154eb01a
ndrs <szaki1006@gmail.com>
2024-10-02 19:00:01 +0200

    :construction: chore: added Inventory to new registered user

--- server/TERI_api/Service/UserService.cs
+++ server/TERI_api/Service/UserService.cs
@@ -1,26 +1,28 @@
 using TERI_api.Model.DataModel;
-using TERI_api.Service.Interface.Repo;
-using TERI_api.Service.Interface.Serv;
+using TERI_api.Service.Repository;
 
 namespace TERI_api.Service;
 
 public class UserService : IUserService
 {
     private readonly IUserRepository _userRepository;
+    private readonly IInventoryRepository _inventoryRepository;
 
-    public UserService(IUserRepository userRepository)
+    public UserService(IUserRepository userRepository, IInventoryRepository inventoryRepository)
     {
         _userRepository = userRepository;
+        _inventoryRepository = inventoryRepository;
     }
 
-    public User? GetByEmail(string email)
+    public async Task<User?> GetByEmailAsync(string email)
     {
-        return _userRepository.GetByEmail(email);
+        var user = await _userRepository.GetByEmailAsync(email);
+        return user;
     }
 
-    public void Add(string email, string username)
+    public async Task AddAsync(string email, string username)
     {
-        var user = new User
+        var newUser = new User
         {
             RegistrationDate = DateTime.Now,
             Name = username,
@@ -30,6 +32,34 @@ public class UserService : IUserService
             IdentityEmail = email
         };
         
-        _userRepository.Add(user);
+        await _userRepository.AddAsync(newUser);
+        
+        var user = await _userRepository.GetByEmailAsync(email);
+
+        if (user != null)
+        {
+            var inventory = await _inventoryRepository.GetByUserIdAsync(user.Id);
+            
+            inventory.IngredientSlots = new List<InventoryIngredientSlot>();
+            inventory.FoodSlots = new List<InventoryFoodSlot>();
+
+            if (inventory != null)
+            {
+                var ingredientSlot = new InventoryIngredientSlot
+                {
+                    Name = "Ingredient Slot #1",
+                    Ingredients = new List<Ingredient>()
+                };
+
+                var foodSlot = new InventoryFoodSlot
+                {
+                    Name = "Food Slot #2",
+                    Foods = new List<Food>()
+                };
+                
+                inventory.IngredientSlots.Add(ingredientSlot);
+                inventory.FoodSlots.Add(foodSlot);
+            }
+        }
     }
 }
